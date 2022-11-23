78581b1405936359cb5434f1710f8bb22d7fe701
David V. Lu!! <davidvlu@gmail.com>
2022-11-23 09:31:50 -0500

    Travis CI (#2)

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-from metro_db import __version__
-
 extra_test = [
     'pytest>=4',
     'pytest-cov>=2',
@@ -10,7 +8,7 @@ extra_test = [
 setup(
     name='metro_db',
     description='A wrapper around the sqlite3 database for easy database development',
-    version=__version__,
+    version='v0.1.0',
     url='https://github.com/DLu/metro_db',
     author='David V. Lu!!',
     author_email='davidvlu@gmail.com',
