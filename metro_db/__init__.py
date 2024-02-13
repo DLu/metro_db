fff1cc9dc0eb78b2ed9158fd79be490e8351e8db
David V. Lu <davidvlu@gmail.com>
2024-02-12 21:10:14 -0500

    Bump version

--- metro_db/__init__.py
+++ metro_db/__init__.py
@@ -2,4 +2,4 @@ from .sqlite_db import SQLiteDB, DatabaseError
 from .metro_db import MetroDB
 
 __all__ = ['SQLiteDB', 'DatabaseError', 'MetroDB']
-__version__ = 'v0.2.1'
+__version__ = 'v0.3.0'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extra_test = [
 setup(
     name='metro_db',
     description='A wrapper around the sqlite3 database for easy database development',
-    version='v0.2.1',
+    version='v0.3.0',
     url='https://github.com/DLu/metro_db',
     author='David V. Lu!!',
     author_email='davidvlu@gmail.com',
