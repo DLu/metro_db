5a1abe4af548fa46414bb7e9a2d9784b353489c4
David V. Lu <davidvlu@gmail.com>
2024-11-04 14:58:43 -0500

    Arg complete

--- src/metro_db/peek.py
+++ src/metro_db/peek.py
@@ -1,20 +1,44 @@
 import argparse
+import argcomplete
+import pathlib
 from tabulate import tabulate, multiline_formats
 
 from . import SQLiteDB
 
+try:
+    import magic
+    def is_database_file(path):
+        return magic.from_file(path).startswith('SQLite')
+except ModuleNotFoundError:
+    def is_database_file(path):
+        return path.suffix in ['.db', '.sql']
+
+
 multiline_formats['fancy_outline'] = 'fancy_outline'  # Hack to fix tabulate bug
+
+
+def db_path_completer(prefix, **kwargs):
+    current = pathlib.Path(prefix)
+    folder = current.parent
+    pattern = current.name + '*'
+    options = []
+    for option_path in folder.glob(pattern):
+        if option_path.is_file() and is_database_file(option_path):
+            options.append(str(option_path))
+
+    return options
 
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('db_path')
+    parser.add_argument('db_path').completer = db_path_completer
     parser.add_argument('n', nargs='?', default=10, type=int,
                         help='Number of rows of each table to display. -1 for all')
     parser.add_argument('-s', '--style', choices=['simple', 'grid', 'plain', 'fancy_outline'], default='fancy_outline')
+    argcomplete.autocomplete(parser, always_complete_options=False)
     args = parser.parse_args(argv)
 
-    db = SQLiteDB(args.db_path)
+    db = SQLiteDB(args.db_path, uri_query='mode=rw')
     db.infer_database_structure()
 
     for table in db.lookup_all('name', 'sqlite_master', 'WHERE type="table"'):
